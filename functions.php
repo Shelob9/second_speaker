7e143f1a1862b174be99ee88b9401a04ecb1f138
Shelob9 <jpollock412@gmail.com>
2013-06-27 23:02:35 -0400

    update child style/script

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
 * Use if you want to enqueue jQuery in one script file the simple way.
 * If so, remove comment lines from line 18 before add_action
 */
-//replace init form parent with the child one. Make sure to keep current.
+//replace js init fromm parent with the child one. Make sure to keep current.
 function _sf_child_init() {
 	if ( !is_admin() ) :
 			wp_enqueue_script('child-init', get_stylesheet_directory_uri().'/js/_sf_child-init.js', array(), false, true);
@@ -20,6 +20,7 @@ function _sf_child_init() {
 //child style override style sheet
 function _sf_child_style() {
 		if ( !is_admin() ) :
+			wp_enqueue_style('style', get_stylesheet_directory_uri().'/style.css');
 			wp_enqueue_style('child-style', get_stylesheet_directory_uri().'/css/child.css');
 		endif;
 
