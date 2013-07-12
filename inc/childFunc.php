09226275eaa962faba29fdec06c8337c4cff0271
Josh <JPollock412@gmail.com>
2013-07-12 19:47:50 -0400

    impliment skin change

--- inc/childFunc.php
+++ inc/childFunc.php
@@ -20,36 +20,27 @@ function _scc_widgets_init() {
 add_action( 'widgets_init', '_scc_widgets_init' );
 endif; //! _scc_widgets_init exists
 
-$side = get_theme_mod('side', 'light');
-function _sf_theChoice_css($side) {
-	if ($side = 'light') {
+$options = get_option('option_tree');
+$side = $options['skin'];
+function _scc_theChoice_css($side) {
+	if ($side == 'light') {
 		wp_enqueue_style('cloud-city', get_stylesheet_directory_uri().'/css/lightside.css');
 	}
 	else {
 		wp_enqueue_style('cloud-city', get_stylesheet_directory_uri().'/css/darkside.css');
 	}
 }
-add_action('wp_enqueue_scripts', '_sf_theChoice_css');
+add_action('wp_enqueue_scripts', '_scc_theChoice_css');
 
 
-function _sf_theChoice_js($side, $use = '') {
-	if ($side = 'light') {
-		$light_bg_img = get_theme_mod('light_bg_img', 'none');
-		if ($light_bg_img = 'none') {
-			$body_img_url = get_stylesheet_directory_uri().'/images/bg.jpg';
-		}
-		if ($light_bg_img != 'none') {
-			$body_img_url = $light_bg_img;
-		}
+function _scc_theChoice_js($side, $use = '' ) {
+$options = get_option('option_tree');
+	if ($side == 'light') {
+		$body_img_url = $options['light_bg_img'];
+			
 	}
-	if ($side = 'dark') {
-		$dark_bg_img = get_theme_mod('dark_bg_img', 'none');
-		if ($dark_bg_img = 'none') {
-			$body_img_url = get_stylesheet_directory_uri().'/images/bg-invert.jpg';
-		}
-		if ($dark_bg_img != 'none') {
-			$body_img_url = $dark_bg_img;
-		}
+	else {
+		$body_img_url = $dark_bg_img = $options['dark_bg_img'];
 	}
 		if (! $use == 'reinit') {
 			echo '<script>';
@@ -61,5 +52,7 @@ function _sf_theChoice_js($side, $use = '') {
 			echo '</script>';
 		}
 }
-add_action('wp_footer', '_sf_theChoice_js');
+add_action('wp_footer', '_scc_theChoice_js');
+
+
 ?>
