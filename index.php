69abe7024c0ee1575fe3882e954199c1e9df5116
Josh <JPollock412@gmail.com>
2013-07-12 19:36:12 -0400

    add index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package _sf
+ */
+get_header(); 
+$sidebar = get_theme_mod('_sf_default_sidebar');
+_sf_open($sidebar);
+?>
+		<?php _sf_home_slider(); ?>
+		<?php if ( have_posts() ) : ?>
+		<?php
+		if ( get_theme_mod( '_sf_masonry' ) == '' ) {
+			echo '<div id="masonry-loop"><!--start masonry-loop-->';
+				/* Start the Loop */
+				while ( have_posts() ) : the_post();
+				get_template_part( 'content', 'masonry' );
+				endwhile;
+			echo '</div><!--end masonry-loop-->';
+			}
+		else {
+			/* Start the Loop */
+			while ( have_posts() ) : the_post();
+			get_template_part( 'content', get_post_format() );
+			endwhile;
+		} ?>
+			<?php _sf_content_nav( 'nav-below' ); ?>
+		<?php else : ?>
+			<?php get_template_part( 'no-results', 'index' ); ?>
+		<?php endif; ?>
+		
+<?php _sf_close($sidebar); ?>
