244094670d5c8635557fa3e7d41e25b204d3765a
Josh <JPollock412@gmail.com>
2013-07-12 20:52:29 -0400

    get sidebar right and use h5 not h1 for widget titles

--- sidebar.php
+++ sidebar.php
@@ -10,18 +10,18 @@
 	_sf_sidebar_starter($sidebar);
 ?>
 		<?php do_action( 'before_sidebar' ); ?>
-		<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
+		<?php if ( ! dynamic_sidebar( 'sidebar-cc' ) ) : ?>
 
 
 			<aside id="archives" class="widget">
-				<div class="widget-title"><h1><?php _e( 'Archives', '_s' ); ?></h1></div>
+				<div class="widget-title"><h5><?php _e( 'Archives', '_s' ); ?></h5></div>
 				<ul>
 					<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
 				</ul>
 			</aside>
 
 			<aside id="meta" class="widget">
-				<div class="widget-title"><h1><?php _e( 'Meta', '_s' ); ?></h1></div>
+				<div class="widget-title"><h5><?php _e( 'Meta', '_s' ); ?></h5></div>
 				<ul>
 					<?php wp_register(); ?>
 					<li><?php wp_loginout(); ?></li>
