78b38d7f828c197b491665d9ccf41237f1c3294d
Josh <JPollock412@gmail.com>
2013-07-25 18:21:06 -0400

    rearranging thumbnail in content.php

--- content.php
+++ content.php
@@ -1,10 +1,19 @@
 <?php
 /**
- * @package _sf
+ * @package _sf 1.1.0
+ * @package gehten since 0.1
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('row'); ?>>
+	<?php if ( has_post_thumbnail()) { ?>
+		<div class="large-3 columns">
+			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" ><?php the_post_thumbnail('small'); ?></a>
+		</div>
+		<div class="large-9 columns">
+		<?php } else { ?> 
+		<div class="large-12 columns">
+		<?php } ?>
 	<header class="entry-header">
 		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', '_sf' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 
@@ -21,11 +30,7 @@
 	</div><!-- .entry-summary -->
 	<?php else : ?>
 	<div class="entry-content">
-		<?php if ( has_post_thumbnail()) : ?>
-			<div class="large-3 columns">
-			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" ><?php the_post_thumbnail('small'); ?></a>
-		</div>
-		<?php endif; ?> 
+		
 		<?php the_excerpt(); ?>
 		<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', '_sf' ), 'after' => '</div>' ) ); ?>
 	</div><!-- .entry-content -->
