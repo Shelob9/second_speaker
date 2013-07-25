d644f5ecaa4360b376922703d6a39167abdebb06
Josh <JPollock412@gmail.com>
2013-07-25 18:40:15 -0400

    content-image if !is_single

--- content-image.php
+++ content-image.php
@@ -3,6 +3,36 @@
  * @package _sf 1.1.0
  * @package gethen since 0.1
  */
+
+if (! is_single() ) {
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class('row'); ?>>
+	<?php if ( has_post_thumbnail()) { ?>
+		<div class="row">
+			<div class="large-12 columns">
+			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" ><?php the_post_thumbnail('large'); ?></a>
+			</div>
+		</div>
+		<div class="row">
+			<div class="large-12 columns">
+				<header class="entry-header">
+					<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', '_sf' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+				</header>
+				<?php the_content(); ?>
+			</div>
+	</article>
+	<?php } else { ?>
+		<div class="large-12 columns">
+			<header class="entry-header">
+			<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', '_sf' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+			</header>
+			<?php the_content(); ?>
+		</div>
+	</article>
+<?php	
+	}//has_post_thumbnail	
+} else {
+//else if isn't single 
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -62,3 +92,5 @@
 		<?php edit_post_link( __( 'Edit', '_sf' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-meta -->
 </article><!-- #post-## -->
+<?php } //end of the else isn't single
+?>
