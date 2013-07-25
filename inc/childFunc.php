11c71735068b39715692b317b92c8db076011bb5
Josh <JPollock412@gmail.com>
2013-07-24 20:20:36 -0400

    function to Declare a global variable for option tree options as $options

--- inc/childFunc.php
+++ inc/childFunc.php
@@ -3,16 +3,28 @@
 * I am a good place to put new functions for the child theme.
 */
 
-global $options;
-$options = get_option('option_tree');
+/**
+* Decalare a global variable for option tree options as $options
+*
+* @since gethen 0.1
+*/
+if (! function_exists('gethen_global_options') ) :
+function gethen_global_options() {
+	global $options;
+	$options = get_option('option_tree');
+}
+add_action('init', 'gethen_global_options');
+endif; // ! gethen_global_options exists
+
 
 /**
 * Set Header Background IMAGE
 *
 *@since Gethen 0.1
 */
-if ( function_exists('gethen_headerBG') ) :
-function gethen_headerBG($options) {
+if (! function_exists('gethen_headerBG') ) :
+function gethen_headerBG() {
+	global $options;
 	if ($options['header_img'] != '' ) {
 		$header_img = $options['header_img'];
 	}
