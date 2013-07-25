e386ceace59cb6795ac23635be5823c019ff5439
Josh <JPollock412@gmail.com>
2013-07-24 20:38:06 -0400

    spell declare right

--- inc/childFunc.php
+++ inc/childFunc.php
@@ -4,7 +4,7 @@
 */
 
 /**
-* Decalare a global variable for option tree options as $options
+* Declare a global variable for option tree options as $options
 *
 * @since gethen 0.1
 */
@@ -16,24 +16,41 @@ function gethen_global_options() {
 add_action('init', 'gethen_global_options');
 endif; // ! gethen_global_options exists
 
-
 /**
 * Set Header Background IMAGE
 *
 *@since Gethen 0.1
 */
 if (! function_exists('gethen_headerBG') ) :
-function gethen_headerBG() {
+function gethen_headerBG($use = '') {
 	global $options;
+	//set the value for the header image.
 	if ($options['header_img'] != '' ) {
 		$header_img = $options['header_img'];
 	}
 	else {
 		$header_img = get_stylesheet_directory_uri().'/images/default-header.jpg';
 	}
-	echo '<script>
-		jQuery.(#full-header).backstretch("'.$header_img.'");
+	//set the value for the body background image
+	if ($options['skin'] != 'skin2') {
+		$body_img_url = $options['sk1_bg_img'];
+	}
+	else {
+		$body_img_url = $options['sk2_bg_img'];
+	}
+	if ($body_img_url == '' ) {
+		$body_img_url = get_stylesheet_directory_uri().'/images/bg.jpg';
+	}
+	if (! $use == 'reinit') {
+		echo '<script>     ';
+	}
+	echo '
+			jQuery("#full-header").backstretch("'.$header_img.'");
+			jQuery.backstretch("'.$body_img_url.'");
 	';
+	if (! $use == 'reinit') {
+		echo '</script>';
+	}
 }
 add_action('wp_footer', 'gethen_headerBG');
 endif; //! gethen_headerBG exists
