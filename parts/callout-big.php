9915886c3d8703e7fbd846ddd4eb51aedeb72472
Josh <JPollock412@gmail.com>
2013-07-25 22:59:24 -0400

    get content for big callout part

--- parts/callout-big.php
+++ parts/callout-big.php
@@ -5,20 +5,27 @@
  * @package geth
  * @since 0.1
  */
+global $options;
 ?>
 <div class="row" id="big-callout-row">
 	<div class="large-12 columns" id="big-callout">
 		<div class="row">
 			<div class="large-3 columns large-offset-1" id="big-callout-image">
-				<img src="http://placekitten.com/600/800" />
+				<img src="<?php esc_html($options['big_callout_img']); ?>" />
 			</div>
 			<div class="large-6 columns">
-				<h5 class="callout-title">Callout Title</h5>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mi justo, bibendum non consectetur quis, adipiscing sit amet nulla. Praesent nibh libero, consequat eget sodales nec, varius ut tellus. Nunc varius feugiat nisl, nec accumsan elit lobortis id. Duis aliquet luctus mi, id commodo augue suscipit aliquam. Sed aliquam tellus vitae leo ultricies ut posuere sem vestibulum. Pellentesque gravida egestas nisl ac ullamcorper. Morbi nec ipsum lectus, sit amet feugiat nisl.</p>
+				<h5 class="callout-title">
+					<?php esc_textarea($options['big_callout_title']); ?>
+				</h5>
+				<?php esc_html($options['big_callout_content']); ?>
 			</div>
 		</div>
 		<div class="row">
-			<div class="large-2 columns" style=float:right;"><a href="#" class="button" id="cta-button">CTA</a></div>
+			<div class="large-2 columns" style=float:right;">
+				<a href="<?php esc_html($options['big_callout_ctaLink']); ?>" class="button" id="cta-button">
+					<?php esc_textarea($options['big_callout_ctaLabel']); ?>
+				</a>
+			</div>
 		</div>
 	</div>
 </div>
