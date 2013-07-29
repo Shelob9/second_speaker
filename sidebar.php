dcc9eea2f86e747ecd24cfbcaa91b435a8d975ed
Josh <JPollock412@gmail.com>
2013-07-28 23:24:23 -0400

    add seidebar.php

---
+++ sidebar.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The Sidebar containing the main widget areas.
+ *
+ * @package _sf
+ */
+?>
+<?php 
+	$sidebar = get_theme_mod('_sf_default_sidebar');
+	_sf_sidebar_starter($sidebar);
+?>
+		<?php do_action( 'before_sidebar' ); ?>
+		<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
+
+			<aside id="search" class="widget widget_search">
+				<?php get_search_form(); ?>
+			</aside>
+
+			<aside id="archives" class="widget">
+				<h1 class="widget-title"><?php _e( 'Archives', '_s' ); ?></h1>
+				<ul>
+					<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
+				</ul>
+			</aside>
+
+			<aside id="meta" class="widget">
+				<h1 class="widget-title"><?php _e( 'Meta', '_s' ); ?></h1>
+				<ul>
+					<?php wp_register(); ?>
+					<li><?php wp_loginout(); ?></li>
+					<?php wp_meta(); ?>
+				</ul>
+			</aside>
+
+		<?php endif; // end sidebar widget area ?>
+	</div><!-- #secondary -->
