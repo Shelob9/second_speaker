a6d1a577c0261027824e9d238d8b97dd668e8423
Josh <JPollock412@gmail.com>
2013-07-29 18:35:58 -0400

    make dite-info/desc right

--- parts/top.php
+++ parts/top.php
@@ -9,8 +9,8 @@
 <header class="full-row" id="full-header">
 <?php do_action( 'tha_header_top' ); ?>
 		<hgroup class="large-8 columns large-centered" id="site-meta">
-			<h1 class="site-title">Site Title</h1>
-			<h2 class="site-description">Site Description ipsum noredium.</h2>
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</hgroup>
 <?php do_action( 'tha_header_bottom' ); ?>
 </header>
