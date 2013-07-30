f9e4388cde83f5da57e04ff7713a84c4f0fae795
Josh <JPollock412@gmail.com>
2013-07-30 19:58:55 -0400

    single.php <?php get_template_part( 'content', get_post_format(), 'single' ); ?>

--- single.php
+++ single.php
@@ -11,11 +11,7 @@ _sf_open($sidebar);
 	
 
 		<?php while ( have_posts() ) : the_post(); ?>
-
-			<?php get_template_part( 'content', 'single' ); ?>
-
-			
-
+			<?php get_template_part( 'content', get_post_format(), 'single' ); ?>
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template
 				if ( comments_open() || '0' != get_comments_number() )
