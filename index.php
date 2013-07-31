71f18107e4557f68a99331588a4a775e4098d834
Josh <JPollock412@gmail.com>
2013-07-30 20:27:56 -0400

    no masonry loop in index

--- index.php
+++ index.php
@@ -17,20 +17,11 @@ _sf_open($sidebar);
 		<?php _sf_home_slider(); ?>
 		<?php if ( have_posts() ) : ?>
 		<?php
-		if ( get_theme_mod( '_sf_masonry' ) == '' ) {
-			echo '<div id="masonry-loop"><!--start masonry-loop-->';
-				/* Start the Loop */
-				while ( have_posts() ) : the_post();
-				get_template_part( 'content', 'masonry' );
-				endwhile;
-			echo '</div><!--end masonry-loop-->';
-			}
-		else {
 			/* Start the Loop */
 			while ( have_posts() ) : the_post();
 			get_template_part( 'content', get_post_format() );
 			endwhile;
-		} ?>
+		?>
 			
 		<?php else : ?>
 			<?php get_template_part( 'no-results', 'index' ); ?>
